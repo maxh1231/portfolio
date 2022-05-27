3098131fc5359d95c3d5c5b42516c9fe91d0c391
maxh1231 <maxhumpherys@gmail.com>
2022-05-27 14:37:27 -0600

    companies

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,7 +1,25 @@
 const Resume = () => {
     return (
         <section>
-            <p>resume</p>
+            <div>
+                <div>
+                    <span>Bank of America</span>
+                </div>
+
+                <div>
+                    <span>2U Inc.</span>
+                </div>
+                <div>
+                    <span>Teleperformance</span>
+                </div>
+                <div>
+                    <span>Discover</span>
+                </div>
+            </div>
+
+            <div>
+
+            </div>
         </section>
     )
 }
