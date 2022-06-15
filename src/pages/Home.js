9d07ddb7d9b2275942d567f3ae6b8f295d0a1ae8
maxh1231 <maxhumpherys@gmail.com>
2022-06-15 17:28:27 -0600

    created Projects component with imports and exports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
-import About from "../components/About";
-import Resume from "../components/Resume";
 import Nav from "../components/Nav"
+import About from "../components/About";
+import Projects from "../components/Projects";
+import Resume from "../components/Resume";
 
 const Home = () => {
     return (
