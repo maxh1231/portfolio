856dfb4bc7c2e272703632aa13ff0fb036ff0458
maxh1231 <maxhumpherys@gmail.com>
2022-06-15 17:21:45 -0600

    created nav component with imports and exports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import About from "../components/About";
 import Resume from "../components/Resume";
+import Nav from "../components/Nav"
 
 const Home = () => {
     return (
         <section>
+            <Nav />
+
             <About />
             <Resume />
         </section>
