e99df629b6beac7253d0f589d2bf2f8836110f86
maxh1231 <maxhumpherys@gmail.com>
2022-06-16 17:41:51 -0600

    fixed resolve error and included projects in render

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import Nav from "../components/Nav"
 import Landing from "../components/Landing";
 import About from "../components/About";
-import Experience from "../components/Resume";
+import Experience from "../components/Experience";
 import Projects from "../components/Projects";
 
 const Home = () => {
@@ -12,6 +12,7 @@ const Home = () => {
 
             <About />
             <Experience />
+            <Projects />
         </section>
     )
 }
