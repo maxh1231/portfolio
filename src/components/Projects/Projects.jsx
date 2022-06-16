3d1588198ea51867ff2d4d00f16ac28133928d20
maxh1231 <maxhumpherys@gmail.com>
2022-06-16 17:54:42 -0600

    added html structure and rough draft of t++ description

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,18 @@
 const Projects = () => {
     return (
         <section>
+            <div>
+                <div>
+                    image
+                </div>
+                <div>
+                    <div>Type++</div>
+                    <div>
+                        This project was developed in the MERN stack. A gamified typing tester, this application can be used simply to test your typing speed, or take it to the next level with achievements, levels, and badges. Built with several packages including graphQL, JWT (JSON WEB Token), Tailwind, ChartJS and more.
+                    </div>
+                    <div>React.js MongoDB</div>
+                </div>
+            </div>
 
         </section>
     )
