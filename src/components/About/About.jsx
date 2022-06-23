c0acd0168783a3939d2ccb4b7253398e87c7a247
maxh1231 <maxhumpherys@gmail.com>
2022-06-23 13:14:33 -0600

    about me html

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,14 @@
 const About = () => {
     return (
         <section id="about">
-            <p>THIS IS ABOUT ME</p>
+            <div>
+                <h2>About Me</h2>
+            </div>
+            <div>
+                <p>
+                    Full Stack Web Developer with a passion to code and furthering my knowledge in all programming-related topics. Skilled in HTML, CSS, JavaScript, MongoDB, Express.js, React.js, Node.js and more. Earned a certificate focused in Full Stack Web Development from the University of Utah.
+                </p>
+            </div>
         </section>
     )
 }
