0be5d96e4ca7a2d1053c090aa218e902b0c01cb9
maxh1231 <maxhumpherys@gmail.com>
2022-06-23 13:16:23 -0600

    about me styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,11 @@
 const About = () => {
     return (
-        <section id="about">
+        <section id="about" className="flex flex-col text-left w-[600px]">
             <div>
-                <h2>About Me</h2>
+                <h2 className="text-4xl">About Me</h2>
             </div>
             <div>
-                <p>
+                <p className="text-2xl">
                     Full Stack Web Developer with a passion to code and furthering my knowledge in all programming-related topics. Skilled in HTML, CSS, JavaScript, MongoDB, Express.js, React.js, Node.js and more. Earned a certificate focused in Full Stack Web Development from the University of Utah.
                 </p>
             </div>
