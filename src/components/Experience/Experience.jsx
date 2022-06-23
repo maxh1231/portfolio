8b339720f7502c56de9a27be7358bc2e19f45134
maxh1231 <maxhumpherys@gmail.com>
2022-06-23 14:09:41 -0600

    font sizes

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,10 +8,10 @@ const Experience = () => {
             return (
                 <div className='ml-4'>
                     <div>
-                        <h2>Claims Analyst</h2>
+                        <h2 className='text-2xl'>Claims Analyst</h2>
                     </div>
                     <div>
-                        <p>May 2020 - Present</p>
+                        <p className='text-xl'>May 2020 - Present</p>
                     </div>
                     <div>
                         <p>Investigate customer reported fraud to determine a responsible and appropriate decision. Navigate multiple systems efficiently to assist in the determination process.</p>
@@ -24,10 +24,10 @@ const Experience = () => {
             return (
                 <div className='ml-4'>
                     <div>
-                        <h2>Tutor & Grader</h2>
+                        <h2 className='text-2xl' >Tutor & Grader</h2>
                     </div>
                     <div>
-                        <p>March 2022 - Present</p>
+                        <p className='text-xl'>March 2022 - Present</p>
                     </div>
                     <div>
                         <p>Tutor students that request additional assistance in categories of University of Utah's Full Stack Coding Bootcamp.</p>
@@ -40,10 +40,10 @@ const Experience = () => {
             return (
                 <div className='ml-4'>
                     <div>
-                        <h2>Property Insurance Agent</h2>
+                        <h2 className='text-2xl'>Property Insurance Agent</h2>
                     </div>
                     <div>
-                        <p>August 2019 - March 2020</p>
+                        <p className='text-xl'>August 2019 - March 2020</p>
                     </div>
                     <div>
                         <p>Licensed property insurance agent in 46 states. Assist customers to enroll in Auto, Home and Renters insurance. Service policies by making adjustments per customer requests.</p>
@@ -56,10 +56,10 @@ const Experience = () => {
             return (
                 <div className='ml-4'>
                     <div>
-                        <h2>Account Manager</h2>
+                        <h2 className='text-2xl'>Account Manager</h2>
                     </div>
                     <div>
-                        <p>October 2017 - August 2019</p>
+                        <p className='text-xl'>October 2017 - August 2019</p>
                     </div>
                     <div>
                         <p>Spoke with a multitude of customers with varying circumstances. Providing great customer service to find the best solutions for each individual while maintaining high performance metrics.</p>
