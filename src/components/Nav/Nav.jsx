272cde9a105e443464e912fd42180138ebb2ef11
maxh1231 <maxhumpherys@gmail.com>
2022-06-27 17:19:19 -0600

    nav padding

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,16 +8,16 @@ const Nav = () => {
             <div>
                 <ul className="flex" >
                     <li>
-                        <a href="#about" className="text-2xl">About</a>
+                        <a href="#about" className="text-2xl pl-6">About</a>
                     </li>
                     <li>
-                        <a href="#exp" className="text-2xl">Experience</a>
+                        <a href="#exp" className="text-2xl pl-6">Experience</a>
                     </li>
                     <li>
-                        <a href="#projects" className="text-2xl">Projects</a>
+                        <a href="#projects" className="text-2xl pl-6">Projects</a>
                     </li>
                     <li>
-                        <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit" target="_blank" className="text-2xl">Resume</a>
+                        <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit" target="_blank" className="text-2xl pl-6">Resume</a>
                     </li>
                 </ul>
             </div>
