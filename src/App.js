5d80470a96114006447c0be35f51646fd6c70e46
maxh1231 <maxhumpherys@gmail.com>
2022-06-29 18:23:48 -0600

    added dark blue to tailwind config and App element

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-dark-blue">
       <Home />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': '#03045e'
+      }
+    },
   },
   plugins: [],
 }
