c8ab661aedcfaedcabc7bf71b5ddd13110a8a98c
maxh1231 <maxhumpherys@gmail.com>
2022-06-30 15:16:17 -0600

    styles to numbering and new config color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       colors: {
         'dark-blue': '#0a192f',
         'blue-white': '#caf0f8',
-        'grey': '#8d99ae'
+        'grey': '#8d99ae',
+        'light-blue': '#00b4d8'
       }
     },
   },
