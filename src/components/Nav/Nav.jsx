d2f5a06077cddd4f424bfff7de1ea62400403301
maxh1231 <maxhumpherys@gmail.com>
2022-06-29 18:37:40 -0600

    added blue-white to nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,22 +2,22 @@ const Nav = () => {
     return (
         <header className="flex justify-between">
             <div>
-                <span className="text-4xl">Max Humpherys</span>
+                <span className="text-4xl text-blue-white">Max Humpherys</span>
             </div>
 
             <div>
-                <ul className="flex" >
+                <ul className="flex " >
                     <li>
-                        <a href="#about" className="text-2xl pl-6">About</a>
+                        <a href="#about" className="text-2xl pl-6 text-blue-white">About</a>
                     </li>
                     <li>
-                        <a href="#exp" className="text-2xl pl-6">Experience</a>
+                        <a href="#exp" className="text-2xl pl-6 text-blue-white">Experience</a>
                     </li>
                     <li>
-                        <a href="#projects" className="text-2xl pl-6">Projects</a>
+                        <a href="#projects" className="text-2xl pl-6 text-blue-white">Projects</a>
                     </li>
                     <li>
-                        <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit" target="_blank" className="text-2xl pl-6">Resume</a>
+                        <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit" target="_blank" className="text-2xl pl-6 text-blue-white">Resume</a>
                     </li>
                 </ul>
             </div>
