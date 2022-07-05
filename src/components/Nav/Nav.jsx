90bf57240c2eb443c0f25088b92e315eba06fc76
maxh1231 <maxhumpherys@gmail.com>
2022-07-05 16:31:46 -0600

    hover on nav links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,16 +8,16 @@ const Nav = () => {
             <div>
                 <ul className="flex " >
                     <li>
-                        <a href="#about" className="text-2xl pl-6 text-blue-white"><span className="text-base text-light-blue">01. </span>About</a>
+                        <a href="#about" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue">01. </span>About</a>
                     </li>
                     <li>
-                        <a href="#exp" className="text-2xl pl-6 text-blue-white"><span className="text-base text-light-blue">02. </span>Experience</a>
+                        <a href="#exp" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue">02. </span>Experience</a>
                     </li>
                     <li>
-                        <a href="#projects" className="text-2xl pl-6 text-blue-white"><span className="text-base text-light-blue">03. </span>Projects</a>
+                        <a href="#projects" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue">03. </span>Projects</a>
                     </li>
                     <li>
-                        <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit" target="_blank" className="text-2xl pl-6 text-blue-white"><span className="text-base text-light-blue">04. </span>Resume</a>
+                        <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit" target="_blank" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue">04. </span>Resume</a>
                     </li>
                 </ul>
             </div>
