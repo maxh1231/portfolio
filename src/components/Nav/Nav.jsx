854ca22b61a6ff018bc315b50a7ca5683aa428ad
maxh1231 <maxhumpherys@gmail.com>
2022-07-06 17:43:10 -0600

    fixed nav with and included contact link

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -17,7 +17,10 @@ const Nav = () => {
                         <a href="#projects" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue">03. </span>Projects</a>
                     </li>
                     <li>
-                        <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit" target="_blank" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue">04. </span>Resume</a>
+                        <a href="#contact" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue">03. </span>Contact</a>
+                    </li>
+                    <li>
+                        <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit" target="_blank" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue"></span>Resume</a>
                     </li>
                 </ul>
             </div>
