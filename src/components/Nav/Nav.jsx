253b1d0784b2fdf4601b483546097318341d6ad5
maxh1231 <maxhumpherys@gmail.com>
2022-07-06 17:48:49 -0600

    styles for resume button

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -17,10 +17,10 @@ const Nav = () => {
                         <a href="#projects" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue">03. </span>Projects</a>
                     </li>
                     <li>
-                        <a href="#contact" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue">03. </span>Contact</a>
+                        <a href="#contact" className="text-2xl pl-6 pr-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue">03. </span>Contact</a>
                     </li>
                     <li>
-                        <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit" target="_blank" className="text-2xl pl-6 text-blue-white hover:text-light-blue"><span className="text-base text-light-blue"></span>Resume</a>
+                        <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit" target="_blank" className="text-2xl px-2 py-2 text-light-blue border-2 border-light-blue rounded-md hover:text-light-blue">Resume</a>
                     </li>
                 </ul>
             </div>
