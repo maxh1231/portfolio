b963a80ff91d8c0bdc962098b16a98d38189e58c
maxh1231 <maxhumpherys@gmail.com>
2022-07-07 17:43:44 -0600

    added margin to project titles

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
                         image
                     </div>
                     <div className="flex flex-col">
-                        <div className="text-4xl text-blue-white">Type++</div>
+                        <div className="text-4xl text-blue-white my-4">Type++</div>
                         <div className="text-xl text-grey">
                             This project was developed in the MERN stack. A gamified typing tester, this application can be used simply to test your typing speed, or take it to the next level with achievements, levels, and badges. Built with several packages including graphQL, JWT (JSON WEB Token), Tailwind, ChartJS and more.
                         </div>
@@ -24,7 +24,7 @@ const Projects = () => {
 
                 <div className="flex flex-row text-left mb-8">
                     <div>
-                        <div className="text-4xl text-blue-white">Turtle</div>
+                        <div className="text-4xl text-blue-white my-4">Turtle</div>
                         <div className="text-xl text-grey" >
                             Inspiried by the popular game Wordle. Turtle is a daily word guesser. With multiple modes, play once a day or as much as you'd like. Built in the MERN stack, utilizing Tailwind, PassportJS, graphQl, and more.
                         </div>
@@ -43,7 +43,7 @@ const Projects = () => {
                         image
                     </div>
                     <div>
-                        <div className="text-4xl text-blue-white">#1Fan</div>
+                        <div className="text-4xl text-blue-white my-4">#1Fan</div>
                         <div className="text-xl text-grey">
                             Music application for lovers of music. Create an account and favorite your top artists and albums, viewable from your customizable dashboard. Built with Express, mySQL, Tailwind and more.
                         </div>
