aa3da5ebe67b0c0acb73122ced069e6fcf10b332
maxh1231 <maxhumpherys@gmail.com>
2022-07-13 16:53:14 -0600

    updated margin values on container and sub container

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,14 @@
 const Contact = () => {
     return (
-        <section id="contact" className="mt-72 w-[800px] mx-auto">
-            <div className="text-4xl text-left mb-6">
+        <section id="contact" className="mt-72 w-[800px] mx-auto pb-36">
+            <div className="text-4xl text-left mb-12">
                 <h3 className='text-blue-white'><span className="text-xl text-light-blue">04. </span>Contact</h3>
             </div>
             <div>
-                <p className="text-2xl text-blue-white">Let's collaborate, my inbox is always open</p>
+                <p className="text-2xl text-blue-white">Let's collaborate, my inbox is always open.</p>
             </div>
             <div>
-                <button className="my-8 text-2xl px-2 py-2 text-light-blue border-2 border-light-blue rounded-md hover:text-light-blue">
+                <button className="my-12 text-2xl px-2 py-2 text-light-blue border-2 border-light-blue rounded-md hover:text-light-blue">
                     Email
                 </button>
             </div>
