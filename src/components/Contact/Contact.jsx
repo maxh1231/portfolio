0475fb45a39e8f466e5bc9cf08593d4ab397fb10
maxh1231 <maxhumpherys@gmail.com>
2022-07-13 16:45:16 -0600

    fixed numbering and text color

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,10 +2,10 @@ const Contact = () => {
     return (
         <section id="contact" className="mt-72 w-[800px] mx-auto">
             <div className="text-4xl text-left mb-6">
-                <h3 className='text-blue-white'><span className="text-xl text-light-blue">02. </span>Contact</h3>
+                <h3 className='text-blue-white'><span className="text-xl text-light-blue">04. </span>Contact</h3>
             </div>
             <div>
-                <p className="text-2xl">My inbox is always open.</p>
+                <p className="text-2xl text-blue-white">My inbox is always open.</p>
             </div>
             <div>
                 <button className="text-2xl">
