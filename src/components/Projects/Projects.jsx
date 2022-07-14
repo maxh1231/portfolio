4067d0349b688e0c5120d83c1f8c4f69558c664a
maxh1231 <maxhumpherys@gmail.com>
2022-07-14 17:32:25 -0600

    progress commit

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,7 +4,7 @@ import typeplusplus from '../../assets/type++.JPG'
 
 const Projects = () => {
     return (
-        <section id="projects" className="flex flex-col w-[800px] m-auto mt-72">
+        <section id="projects" className="flex flex-col w-[1200px] m-auto mt-72">
             <div>
                 <h3 className="text-4xl text-left text-blue-white mb-6"><span className="text-xl text-light-blue">03. </span>Projects</h3>
             </div>
@@ -12,11 +12,11 @@ const Projects = () => {
             <div>
                 <div className="flex flex-row text-right mb-8 items-center">
                     <div className=''>
-                        <img src={typeplusplus} alt="image of Type++ project" className='w-[1200px]'></img>
+                        <img src={typeplusplus} alt="image of Type++ project" className='w-[600px] h-[300px]'></img>
                     </div>
                     <div className="flex flex-col">
                         <div className="text-4xl text-blue-white my-4">Type++</div>
-                        <div className="text-xl text-grey">
+                        <div className="relative w-[600px] text-lg text-grey">
                             This project was developed in the MERN stack. A gamified typing tester, this application can be used simply to test your typing speed, or take it to the next level with achievements, levels, and badges. Built with several packages including graphQL, JWT (JSON WEB Token), Tailwind, ChartJS and more.
                         </div>
                         <div className="text-lg">
@@ -29,7 +29,7 @@ const Projects = () => {
                 <div className="flex flex-row text-left mb-8 items-center">
                     <div>
                         <div className="text-4xl text-blue-white my-4">Turtle</div>
-                        <div className="text-xl text-grey" >
+                        <div className="relative text-xl text-grey" >
                             Inspiried by the popular game Wordle. Turtle is a daily word guesser. With multiple modes, play once a day or as much as you'd like. Built in the MERN stack, utilizing Tailwind, PassportJS, graphQl, and more.
                         </div>
                         <div className="text-lg">
@@ -38,17 +38,17 @@ const Projects = () => {
                         </div>
                     </div>
                     <div>
-                        <img src={turtle} alt="image of Turtle project" className='w-[1200px]'></img>
+                        <img src={turtle} alt="image of Turtle project" className=''></img>
                     </div>
                 </div>
 
                 <div className="flex flex-row text-right mb-8 items-center">
                     <div>
-                        <img src={no1fan} alt="image of No. 1 Fan project" className='w-[1200px]'></img>
+                        <img src={no1fan} alt="image of No. 1 Fan project" className=''></img>
                     </div>
                     <div>
                         <div className="text-4xl text-blue-white my-4">#1Fan</div>
-                        <div className="text-xl text-grey">
+                        <div className="relative text-xl text-grey">
                             Music application for lovers of music. Create an account and favorite your top artists and albums, viewable from your customizable dashboard. Built with Express, mySQL, Tailwind and more.
                         </div>
                         <div className="text-lg">
