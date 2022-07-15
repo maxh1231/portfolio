5885b7ac9cdd09842c366bdba5b3b7215e066fa7
maxh1231 <maxhumpherys@gmail.com>
2022-07-14 21:34:15 -0600

    positioned remaining elements relatively

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,33 +10,33 @@ const Projects = () => {
             </div>
 
             <div>
-                <div className="flex flex-row text-right mb-8 items-center">
+                <div className="flex flex-row text-right mb-24 items-center">
                     <div className=" main ">
                         <div className="overlay ">
                             <img src={typeplusplus} alt="image of Type++ project" className=''></img>
                         </div>
                     </div>
                     <div className="flex flex-col ">
-                        <div className="text-4xl text-blue-white my-4">Type++</div>
-                        <div className="relative right-40 top-0 w-[600px] text-lg text-grey bg-dark-blue-bg p-7 rounded-lg bg-dark-blue-bg">
+                        <div className="relative right-40 text-4xl text-blue-white my-4">Type++</div>
+                        <div className="relative right-40 top-0 w-[600px] text-base text-grey bg-dark-blue-bg p-7 rounded-lg bg-dark-blue-bg">
                             This project was developed in the MERN stack. A gamified typing tester, this application can be used simply to test your typing speed, or take it to the next level with achievements, levels, and badges. Built with several packages including graphQL, JWT (JSON WEB Token), Tailwind, ChartJS and more.
                         </div>
                         <div className="text-lg">
-                            <span className="mr-4 text-grey">React.js</span>
-                            <span className=" text-grey"> MongoDB</span>
+                            <span className="relative right-40 mr-4 text-grey">React.js</span>
+                            <span className="relative right-40 text-grey"> MongoDB</span>
                         </div>
                     </div>
                 </div>
 
-                <div className="flex flex-row text-left mb-8 items-center">
+                <div className="flex flex-row text-left mb-24 items-center">
                     <div>
-                        <div className="text-4xl text-blue-white my-4">Turtle</div>
+                        <div className="relative left-40 text-4xl text-blue-white my-4">Turtle</div>
                         <div className="relative left-40 w-[600px] text-lg text-grey bg-dark-blue-bg p-7 rounded-lg z-50" >
                             Inspiried by the popular game Wordle. Turtle is a daily word guesser. With multiple modes, play once a day or as much as you'd like. Built in the MERN stack, utilizing Tailwind, PassportJS, graphQl, and more.
                         </div>
                         <div className="text-lg">
-                            <span className=" text-grey">React.js</span>
-                            <span className="ml-4 text-grey"> MongoDB</span>
+                            <span className="relative left-40 text-grey">React.js</span>
+                            <span className="relative left-40 ml-4 text-grey"> MongoDB</span>
                         </div>
                     </div>
                     <div className='main'>
@@ -53,13 +53,13 @@ const Projects = () => {
                         </div>
                     </div>
                     <div>
-                        <div className="text-4xl text-blue-white my-4">#1Fan</div>
+                        <div className="relative right-40 text-4xl text-blue-white my-4">#1Fan</div>
                         <div className="relative right-40 w-[600px] text-lg text-grey bg-dark-blue-bg p-7 rounded-lg">
                             Music application for lovers of music. Create an account and favorite your top artists and albums, viewable from your customizable dashboard. Built with Express, mySQL, Tailwind and more.
                         </div>
                         <div className="text-lg">
-                            <span className="mr-4 text-grey">HandlebarsJS</span>
-                            <span className=" text-grey"> mySQL</span>
+                            <span className="relative right-40 mr-4 text-grey">HandlebarsJS</span>
+                            <span className="relative right-40 text-grey"> mySQL</span>
                         </div>
                     </div>
                 </div>
