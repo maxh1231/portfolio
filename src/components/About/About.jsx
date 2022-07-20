179c40baa2656e5802207f3aea822c5132ac2004
maxh1231 <maxhumpherys@gmail.com>
2022-07-20 16:42:02 -0600

    MQs for about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,10 +2,10 @@ const About = () => {
     return (
         <section id="about" className="flex flex-col text-left w-[600px] md:w-[800px] m-auto">
             <div className="mb-6">
-                <h2 className="text-4xl text-blue-white"><span className="text-xl text-light-blue">01. </span>About Me</h2>
+                <h2 className="text-2xl md:text-4xl text-blue-white"><span className="text-lg md:text-xl text-light-blue">01. </span>About Me</h2>
             </div>
             <div>
-                <p className="text-2xl text-grey">
+                <p className="md:text-2xl text-grey">
                     Full Stack Web Developer with a passion to code and furthering my knowledge in all programming-related topics. Skilled in HTML, CSS, JavaScript, MongoDB, Express.js, React.js, Node.js and more. Earned a certificate focused in Full Stack Web Development from the University of Utah.
                 </p>
             </div>
