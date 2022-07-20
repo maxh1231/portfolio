71ffc83b6a3ee70d3990ec6dfff2241e1ee2798b
maxh1231 <maxhumpherys@gmail.com>
2022-07-20 16:30:05 -0600

    added MQ for landing font size below md size

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,14 +2,14 @@ const Landing = () => {
     return (
         <section className="text-left flex flex-col h-screen justify-center w-[600px] md:w-[800px] m-auto">
             <div>
-                <p className="md:text-4xl text-blue-white ">Hello</p>
+                <p className="text-2xl md:text-4xl text-blue-white ">Hello</p>
             </div>
             <div>
-                <p className="md:text-6xl ">Max Humpherys</p>
-                <p className="md:text-6xl text-grey ">I build web applications</p>
+                <p className="text-4xl md:text-6xl ">Max Humpherys</p>
+                <p className="text-4xl md:text-6xl text-grey ">I build web applications</p>
             </div>
             <div>
-                <p className="md:text-4xl text-grey">
+                <p className="text-xl md:text-4xl text-grey">
                     I'm a full stack web developer that builds and creates exceptional digital experiences.
                 </p>
             </div>
