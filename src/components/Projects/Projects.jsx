5287ed2db7fb82cffe839347760e733b5786a5ca
maxh1231 <maxhumpherys@gmail.com>
2022-08-01 14:26:08 -0600

    MQ changes for MD

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,7 +5,7 @@ import github from '../../assets/github.svg'
 
 const Projects = () => {
     return (
-        <section id="projects" className="flex flex-col w-[150px] xs:w-[300px] sm:w-[600px] md:w-[800px] xl:w-[1200px] mx-auto  mt-72 xl:mx-42">
+        <section id="projects" className="flex flex-col w-[150px] xs:w-[300px] sm:w-[600px] md:w-[800px] lg:w-[1000px] xl:w-[1200px] mx-auto  mt-72 xl:mx-42">
             <div>
                 <h3 className="text-2xl md:text-4xl text-left text-blue-white mb-24"><span className="text-lg md:text-xl text-light-blue">03. </span>Projects</h3>
             </div>
@@ -13,7 +13,7 @@ const Projects = () => {
             <div>
                 <div className="flex flex-col md:flex-row text-right mb-16 items-center">
                     <div className=" main rounded-md ">
-                        <div className="hidden md:block overlay rounded-md ">
+                        <div className="hidden lg:block overlay rounded-md ">
                             <a href="https://typeplusplus.herokuapp.com/" target="_blank">
                                 <img src={typeplusplus} alt="image of Type++ project" className='rounded-md'></img>
                             </a>
@@ -22,15 +22,15 @@ const Projects = () => {
                     <div className="flex flex-col ">
                         <div className="relative md:right-40 text-2xl md:text-4xl text-blue-white my-4">Type++</div>
                         <a href="https://typeplusplus.herokuapp.com/" target="blank" className=''>
-                            <div className="relative md:right-40 top-0 w-[150px] xs:w-[300px] md:w-[600px] text-base text-grey bg-dark-blue-bg p-7 rounded-lg bg-dark-blue-bg">
+                            <div className="relative lg:right-40 top-0 w-[150px] xs:w-[300px] md:w-[600px] text-base text-grey bg-dark-blue-bg p-7 rounded-lg bg-dark-blue-bg">
                                 This project was developed in the MERN stack. A gamified typing tester, this application can be used simply to test your typing speed, or take it to the next level with achievements, levels, and badges. Built with several packages including graphQL, JWT (JSON WEB Token), Tailwind, ChartJS and more.
                             </div>
                         </a>
                         <div className="flex flex-row-reverse items-center relative md:right-40 top-2 text-lg">
                             <span className="text-grey ml-4">React.js</span>
                             <span className="text-grey ml-4"> MongoDB</span>
-                            <a>
-                                <img src={github} alt="" className='github w-[32px] h-[32px]' />
+                            <a href='https://github.com/maxh1231/type-Plus-Plus' target="_blank">
+                                <img src={github} alt="github logo" className='github w-[32px] h-[32px]' />
                             </a>
                         </div>
                     </div>
@@ -40,20 +40,20 @@ const Projects = () => {
                     <div>
                         <div className="relative md:left-40 text-2xl md:text-4xl text-blue-white my-4">Turtle</div>
                         <a href="https://wurtle.herokuapp.com/" target="_blank" className="">
-                            <div className="relative md:left-40 w-[150px] xs:w-[300px] md:w-[600px] text-base text-grey bg-dark-blue-bg p-7 rounded-lg z-50" >
+                            <div className="relative lg:left-40 w-[150px] xs:w-[300px] md:w-[600px] text-base text-grey bg-dark-blue-bg p-7 rounded-lg z-50" >
                                 Inspiried by the popular game Wordle. Turtle is a daily word guesser. With multiple modes, play once a day or as much as you'd like. Built in the MERN stack, utilizing Tailwind, PassportJS, graphQl, and more.
                             </div>
                         </a>
                         <div className="flex tems-center md:pl-40 relative top-2 text-lg">
                             <span className="text-grey mr-4">React.js</span>
                             <span className="text-grey mr-4"> MongoDB</span>
-                            <a>
-                                <img src={github} alt="" className='github w-[32px] h-[32px]' />
+                            <a href='https://github.com/maxh1231/turtle' target="_blank">
+                                <img src={github} alt="github logo" className='github w-[32px] h-[32px]' />
                             </a>
                         </div>
                     </div>
                     <div className='main rounded-md'>
-                        <div className="hidden md:block overlay rounded-md">
+                        <div className="hidden lg:block overlay rounded-md">
                             <a href="https://wurtle.herokuapp.com/" target="_blank">
                                 <img src={turtle} alt="image of Turtle project" className='rounded-md'></img>
                             </a>
@@ -63,7 +63,7 @@ const Projects = () => {
 
                 <div className="flex flex-col md:flex-row text-right mb-8 items-center">
                     <div className='main rounded-md'>
-                        <div className="hidden md:block overlay rounded-md">
+                        <div className="hidden lg:block overlay rounded-md">
                             <a href="https://onlybands.herokuapp.com/" target="_blank">
                                 <img src={no1fan} alt="image of No. 1 Fan project" className=' rounded-md'></img>
                             </a>
@@ -72,15 +72,15 @@ const Projects = () => {
                     <div>
                         <div className="relative md:right-40 text-2xl md:text-4xl text-blue-white my-4">#1Fan</div>
                         <a href="https://onlybands.herokuapp.com/" target="_blank" className="">
-                            <div className="relative md:right-40 w-[150px] xs:w-[300px] md:w-[600px] text-base text-grey bg-dark-blue-bg p-7 rounded-lg">
+                            <div className="relative lg:right-40 w-[150px] xs:w-[300px] md:w-[600px] text-base text-grey bg-dark-blue-bg p-7 rounded-lg">
                                 Music application for lovers of music. Create an account and favorite your top artists and albums, viewable from your customizable dashboard. Built with Express, mySQL, Tailwind and more.
                             </div>
                         </a>
                         <div className="flex flex-row-reverse items-center relative md:right-40 top-2 text-lg">
                             <span className="text-grey ml-4">HandlebarsJS</span>
                             <span className="text-grey ml-4"> mySQL</span>
-                            <a>
-                                <img src={github} alt="" className='github w-[32px] h-[32px]' />
+                            <a href='https://github.com/maxh1231/no1fan' target="_blank">
+                                <img src={github} alt="github logo" className='github w-[32px] h-[32px]' />
                             </a>
                         </div>
                     </div>
