676026e13c1617a5b8a2ad6efa3b074eeba1327d
maxh1231 <maxhumpherys@gmail.com>
2022-08-03 14:54:00 -0600

    added skills and map function to display each item

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -5,11 +5,10 @@ const Landing = () => {
         <section className="text-center flex flex-col md:flex-row md:text-left items-center h-screen justify-center w-[200px] sm:w-[400px] md:w-[600px] lg:w-[800px] m-auto">
             <div>
                 <div>
-                    <p className="text-2xl md:text-4xl text-blue-white ">Hello</p>
+                    <p className="text-2xl md:text-4xl text-blue-white ">Hello,</p>
                 </div>
                 <div>
-                    <p className="text-4xl md:text-6xl ">Max Humpherys</p>
-                    <p className="text-4xl md:text-6xl text-grey ">I build web applications</p>
+                    <p className="text-4xl md:text-6xl text-blue-white ">I build web applications</p>
                 </div>
                 <div>
                     <p className="text-xl md:text-4xl text-grey">
