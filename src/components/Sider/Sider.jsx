011156bfd4e870c76d2ef825a83a69daa82bbc84
maxh1231 <maxhumpherys@gmail.com>
2022-08-03 16:20:50 -0600

    added side bar with email

---
+++ src/components/Sider/Sider.jsx
@@ -0,0 +1,9 @@
+const Sider = () => {
+    return (
+        <section className="footer fixed bottom-[210px] h-[200px]">
+            <p className="social text-lg text-grey ">maxhumpherys@gmail.com</p>
+        </section>
+    )
+}
+
+export default Sider;
