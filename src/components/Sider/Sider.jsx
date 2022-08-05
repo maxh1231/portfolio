95c8bff5ee4202af32abcc8718866693fd621a58
maxh1231 <maxhumpherys@gmail.com>
2022-08-05 16:48:46 -0600

    added a tags for respective links

--- src/components/Sider/Sider.jsx
+++ src/components/Sider/Sider.jsx
@@ -5,10 +5,16 @@ const Sider = () => {
     return (
         <section className="footer fixed bottom-[210px] h-[200px]">
             <div className='flex flex-col'>
-                <img src={github} alt="github" className='social-left github w-[32px] h-[32px] mb-4' />
-                <img src={linkedin} alt="github" className='social-left github w-[32px] h-[32px] mb-4' />
+                <a href="https://github.com/maxh1231" target="_blank">
+                    <img src={github} alt="github" className='social-left github w-[32px] h-[32px] mb-4' />
+                </a>
+                <a href="https://www.linkedin.com/in/max-humpherys-040822219/" target="_blank">
+                    <img src={linkedin} alt="github" className='social-left github w-[32px] h-[32px] mb-4' />
+                </a>
             </div>
-            <p className="social text-lg text-grey ">maxhumpherys@gmail.com</p>
+            <a href="mailto: maxhumpherys@gmail.com">
+                <p className="social text-lg text-grey ">maxhumpherys@gmail.com</p>
+            </a>
         </section>
     )
 }
