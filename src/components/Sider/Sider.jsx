a7fb67130f84b6571e54acb39c2bc923a1a9562f
maxh1231 <maxhumpherys@gmail.com>
2022-08-05 16:37:25 -0600

    added linkedin logo to container

--- src/components/Sider/Sider.jsx
+++ src/components/Sider/Sider.jsx
@@ -4,7 +4,10 @@ import linkedin from '../../assets/linkedin.svg'
 const Sider = () => {
     return (
         <section className="footer fixed bottom-[210px] h-[200px]">
-            <img src={github} alt="github" className='social-left github w-[32px] h-[32px]' />
+            <div className='flex flex-col'>
+                <img src={github} alt="github" className='social-left github w-[32px] h-[32px] mb-4' />
+                <img src={linkedin} alt="github" className='social-left github w-[32px] h-[32px] mb-4' />
+            </div>
             <p className="social text-lg text-grey ">maxhumpherys@gmail.com</p>
         </section>
     )
