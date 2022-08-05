a7543f86a8bf95c9c7c7465e407a20ec2401de38
maxh1231 <maxhumpherys@gmail.com>
2022-08-05 17:01:46 -0600

    misc changes

--- src/components/Sider/Sider.jsx
+++ src/components/Sider/Sider.jsx
@@ -5,15 +5,15 @@ const Sider = () => {
     return (
         <section className="footer fixed bottom-[210px] h-[200px]">
             <div className='flex flex-col'>
-                <a href="https://github.com/maxh1231" target="_blank">
-                    <img src={github} alt="github" className='social-left github w-[32px] h-[32px] mb-4' />
+                <a href="https://github.com/maxh1231" target="_blank" className='w-[32px] h-[32px] mb-4'>
+                    <img src={github} alt="github" className='social-left github ' />
                 </a>
-                <a href="https://www.linkedin.com/in/max-humpherys-040822219/" target="_blank">
-                    <img src={linkedin} alt="github" className='social-left github w-[32px] h-[32px] mb-4' />
+                <a href="https://www.linkedin.com/in/max-humpherys-040822219/" target="_blank" className='w-[32px] h-[32px] mb-4'>
+                    <img src={linkedin} alt="github" className='social-left github' />
                 </a>
             </div>
-            <a href="mailto: maxhumpherys@gmail.com">
-                <p className="social text-lg text-grey ">maxhumpherys@gmail.com</p>
+            <a href="mailto: maxhumpherys@gmail.com" className='social'>
+                <span className=" text-lg text-grey ">maxhumpherys@gmail.com</span>
             </a>
         </section>
     )
