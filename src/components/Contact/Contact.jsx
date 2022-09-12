1d2183caebe7932298679ee872ab295d247c950b
maxh1231 <maxhumpherys@gmail.com>
2022-09-12 15:41:21 -0600

    package updates and basic contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,25 @@
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { useForm } from 'react-hook-form';
+
 const Contact = () => {
+    const [view, setView] = useState(false);
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+
+        emailjs.sendForm('service_zirnhu8', 'template_z5s2y9b', form.current, 'user_MiPMA7RruofT1yJEswkdw')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text)
+            });
+
+        setView(true);
+    };
+
     return (
         <section id="contact" className="mt-72 w-[200px] sm:w-[400px] md:w-[600px] lg:w-[800px] mx-auto pb-36 z-[100]">
             <div className="text-2xl md:text-4xl text-left mb-12">
@@ -8,11 +29,19 @@ const Contact = () => {
                 <p className="text-2xl text-grey">Let's collaborate, my inbox is always open.</p>
             </div>
             <div>
-                <a href="mailto: maxhumpherys@gmail.com">
-                    <button className="my-12 text-2xl px-2 py-2 text-light-blue border-2 border-light-blue rounded-md hover:text-light-blue hover:bg-light-grey">
-                        Email
-                    </button>
-                </a>
+                <form className='flex flex-col'>
+                    <div>
+                        <input type="text" placeholder='Name'></input>
+                    </div>
+                    <div>
+                        <textarea placeholder='Message'></textarea>
+                    </div>
+                    <a href="mailto: maxhumpherys@gmail.com">
+                        <button className="my-12 text-2xl px-2 py-2 text-light-blue border-2 border-light-blue rounded-md hover:text-light-blue hover:bg-light-grey">
+                            Email
+                        </button>
+                    </a>
+                </form>
             </div>
         </section>
     )
