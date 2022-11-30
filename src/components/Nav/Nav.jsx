d2027ba5215b52e795b1d9f189c04b5095eed21a
maxh1231 <maxhumpherys@gmail.com>
2022-11-29 22:43:52 -0700

    updated resume link

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -22,7 +22,7 @@ const Nav = () => {
                         <a href="#contact" className="hidden sm:block text-lg lg:text-2xl md:pl-6 md:pr-6 text-blue-white hover:text-light-blue"><span className="text-xs sm:text-base text-light-blue">04. </span>Contact</a>
                     </li>
                     <li className="ml-2 mt-3 md:ml-0 md:mt-0">
-                        <a href="https://1drv.ms/b/s!ArCjh7nOvAwSlADfp4xFRUnea2t0?e=cJbanT" target="_blank" className="text-xl lg:text-2xl px-2 py-2 text-light-blue border-2 border-light-blue rounded-md hover:text-light-blue hover:bg-light-grey ">Resume</a>
+                        <a href="https://1drv.ms/b/s!ArCjh7nOvAwSlAVpUGDJvrefq9OA?e=vj23fw" target="_blank" className="text-xl lg:text-2xl px-2 py-2 text-light-blue border-2 border-light-blue rounded-md hover:text-light-blue hover:bg-light-grey ">Resume</a>
                     </li>
                 </ul>
             </div>
