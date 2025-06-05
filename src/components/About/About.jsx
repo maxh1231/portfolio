68f405364c4856e57080278e9b3a238bb088c293
max <maxhumpherys@gmail.com>
2025-06-04 23:07:35 -0600

    update landing and about descriptions

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,35 +1,86 @@
-const skills = ["JavaScript", "TypeScript", "Node.js", "React.js", "Express.js", "Git", "Jest", "MongoDB", "Mongoose", "Vite/Vercel"]
+const skills = [
+    'JavaScript',
+    'TypeScript',
+    'React.js',
+    'Java',
+    'PHP',
+    'C',
+    'SQL',
+    'MySQL',
+    'PostreSQL',
+];
 
-const moreSkills = ["mySQL", "Sequelize", "PostgreSQL", "graphQL", "Apollo Client", "CSS3", "Figma", "Tailwind", "Bootstrap", "And more!"]
+const moreSkills = [
+    'GraphQL',
+    'MongoDB',
+    'Docker',
+    'Git',
+    'Spring',
+    'Bash',
+    'Linux CLI Tools',
+    'CSS3',
+    'Tailwind',
+];
 
 const About = () => {
     return (
-        <section id="about" className="flex flex-col text-left w-[250px] sm:w-[400px] md:w-[600px] lg:w-[800px] m-auto">
-            <div className="flex items-center mb-6 xs:mb-24">
-                <h2 className="text-2xl md:text-4xl text-blue-white"><span className="text-lg md:text-xl text-light-blue">01. </span>About Me <span className="hidden sm:inline relative top-6 left-4 text-dark-blue border-t border-grey">___________________</span></h2>
+        <section
+            id='about'
+            className='flex flex-col text-left w-[250px] sm:w-[400px] md:w-[600px] lg:w-[800px] m-auto'
+        >
+            <div className='flex items-center mb-2 xs:mb-10'>
+                <h2 className='text-2xl md:text-4xl text-blue-white'>
+                    <span className='text-lg md:text-xl text-light-blue'>
+                        01.{' '}
+                    </span>
+                    About Me{' '}
+                    <span className='hidden sm:inline relative top-6 left-4 text-dark-blue border-t border-grey'>
+                        ___________________
+                    </span>
+                </h2>
             </div>
             <div>
-                <p className="md:text-2xl text-grey">
-                    I'm a dedicated Full Stack Web Developer fueled by a passion for coding and a relentless pursuit of knowledge in the realm of programming. Armed with a Full Stack Web Development certificate from The University of Utah and an Associate of Science degree in Computer Science from Salt Lake Community College, I'm on a quest for continuous growth. Beyond coding, I'm an avid enthusiast of movies, music, and video games, and my faithful coding companion is none other than my feline friend, Rufus.
+                <p className='md:text-2xl text-grey'>
+                    With an Associate's degree in Computer Science and
+                    Information Systems and a Full Stack Web Development
+                    Certificate, I’ve developed a strong foundation in both
+                    theoretical and practical software development. My
+                    experience spans front-end and back-end technologies,
+                    including JavaScript, TypeScript, Java, PHP, SQL/NoSQL
+                    databases, and cloud architecture. I’m dedicated to
+                    continuous learning and growth with evolving technologies by
+                    exploring new frameworks and deepening my understanding of
+                    computer science fundamentals. Outside of work, I enjoy
+                    movies, music, and chess—and I’m rarely coding without my
+                    feline friend, Rufus, nearby.
                 </p>
             </div>
             <div>
-                <h3 className="text-2xl text-blue-white my-4">Skills and Qualifications:</h3>
-                <div className="flex">
-                    <div className="mr-2">
+                <h3 className='text-2xl text-blue-white my-4'>
+                    Skills and Qualifications:
+                </h3>
+                <div className='flex'>
+                    <div className='mr-2'>
                         {skills.map((item, index) => (
-                            <p className="text-grey" key={index}> &#62; {item} </p>
+                            <p className='text-grey' key={index}>
+                                {' '}
+                                &#62; {item}{' '}
+                            </p>
                         ))}
                     </div>
-                    <div className="ml-2">
+                    <div className='ml-2'>
                         {moreSkills.map((item, index) => (
-                            <p className=" text-grey" key={index}> &#62; {item} </p>
+                            <p className=' text-grey' key={index}>
+                                {' '}
+                                &#62; {item}{' '}
+                            </p>
                         ))}
                     </div>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default About;
+
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,39 +1,32 @@
-import avatar from '../../assets/avataaars.svg'
+import avatar from '../../assets/avataaars.svg';
 
 const Landing = () => {
     return (
-        <section className="text-center flex flex-col md:flex-row md:text-left items-center h-screen justify-center w-[200px] sm:w-[400px] md:w-[600px] lg:w-[800px] m-auto">
+        <section className='text-center flex flex-col md:flex-row md:text-left items-center h-screen justify-center w-[200px] sm:w-[400px] md:w-[600px] lg:w-[800px] m-auto'>
             <div className='md:w-[336px] lg:w-[536px]'>
                 <div>
-                    <p className="text-2xl md:text-4xl text-blue-white ">Hello,</p>
+                    <p className='text-2xl md:text-4xl text-blue-white '>
+                        Hello,
+                    </p>
                 </div>
                 <div>
-                    <p className="text-4xl md:text-6xl text-blue-white ">I build web applications</p>
+                    <p className='text-4xl md:text-6xl text-blue-white '>
+                        I build full-stack applications.
+                    </p>
                 </div>
                 <div>
-                    <p className="text-xl md:text-4xl text-grey">
-                        I'm a full stack web developer that builds and creates exceptional digital experiences.
+                    <p className='text-xl md:text-4xl text-grey'>
+                        I'm a software engineer focused on creating robust,
+                        scalable, and user-focused digital solutions.
                     </p>
                 </div>
             </div>
-            <div >
-                {/* <Avatar
-                    avatarStyle='Transparent'
-                    topType='ShortHairShortWaved'
-                    accessoriesType='Prescription02'
-                    hairColor='BrownDark'
-                    facialHairType='Blank'
-                    clotheType='Hoodie'
-                    clotheColor='Blue02'
-                    eyeType='Happy'
-                    eyebrowType='RaisedExcited'
-                    mouthType='Smile'
-                    skinColor='Light'
-                /> */}
-                <img src={avatar} alt="" />
+            <div>
+                <img src={avatar} alt='avatar' />
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Landing;
+
