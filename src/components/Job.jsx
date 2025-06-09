43f9d83c2f588f1c9f1817fd582429605bd5b423
max <maxhumpherys@gmail.com>
2025-06-08 22:32:49 -0600

    experience style cleanup

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,24 +1,20 @@
 const Job = ({ title, company, startDate, endDate, bullets }) => {
     return (
-        <div className='ml-4'>
-            <div>
-                <h2 className=' md:text-2xl text-blue-white'>
+        <div className='flex flex-col min-h-[566px]'>
+            <div className='my-6'>
+                <h2 className='text-xl md:text-2xl text-blue-white'>
                     {title}
                     <span className='text-light-blue'> @ {company}</span>
                 </h2>
-            </div>
-            <div className='mb-6'>
                 <p className='md:text-xl text-grey'>
                     {startDate} - {endDate}
                 </p>
             </div>
-            <div>
-                {bullets.map((item, i) => (
-                    <p className='md:text-xl text-grey mt-4' key={i}>
-                        &#62; {item}
-                    </p>
-                ))}
-            </div>
+            {bullets.map((item, i) => (
+                <p className='md:text-xl text-grey mb-4' key={i}>
+                    &#62; {item}
+                </p>
+            ))}
         </div>
     );
 };
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -6,42 +6,28 @@ const Experience = () => {
     const [currentJob, setCurrentJob] = useState(0);
 
     return (
-        <section className='hidden flex-col mt-72 sm:block sm:w-[400px] md:block md:w-[600px] lg:block lg:w-[800px] m-auto z-[3]'>
-            <div className='text-2xl md:text-4xl text-left mb-6'>
-                <h3 className='text-blue-white'>
-                    <span className='text-lg md:text-xl text-light-blue'>
-                        02.
-                    </span>
-                    Where I've Worked
-                    <span className='relative hidden sm:inline top-6 left-4 text-dark-blue border-t border-grey'>
-                        _____________
-                    </span>
-                </h3>
-            </div>
-            <div id='exp' className='flex justify-center text-left h-[424px]'>
-                <div className='flex flex-col justify-center min-w-[140px]'>
-                    <div
-                        onClick={() => setCurrentJob(0)}
-                        className={`border-l-2 ${currentJob === 0 ? 'border-light-blue' : 'border-grey '} hover:bg-light-grey hover:cursor-pointer py-2 pr-2 pl-4`}
-                    >
+        <section className='max-w-[1000px] px-4 flex-col mt-72 m-auto'>
+            <h2 className='text-3xl md:text-4xl text-left text-blue-white'>
+                <span className='text-lg md:text-xl text-light-blue'>02. </span>
+                Experience
+                <span className='relative hidden sm:inline top-6 left-4 text-dark-blue border-t border-grey'>
+                    ___________________
+                </span>
+            </h2>
+            <div
+                id='exp'
+                className='flex flex-col md:flex-row justify-center text-left'
+            >
+                <div className='flex justify-start items-center md:flex-col md:justify-center mt-2 md:m-0 space-x-4 md:space-x-0 md:mr-4'>
+                    {jobs.map((item, i) => (
                         <span
-                            onClick={() => setCurrentJob(0)}
-                            className={`text-lg md:md:text-xl ${currentJob === 0 ? 'text-light-blue' : 'text-grey'} hover:cursor-pointer`}
+                            key={i}
+                            onClick={() => setCurrentJob(i)}
+                            className={`min-w-[100px] border-b-2 md:border-b-0 md:border-l-2 ${currentJob === i ? 'border-light-blue text-light-blue' : 'border-grey text-grey '} hover:bg-light-grey hover:cursor-pointer p-4 text-lg text-center md:text-xl md:mb-4`}
                         >
-                            {jobs[0].company}
+                            {item.company}
                         </span>
-                    </div>
-                    <div
-                        onClick={() => setCurrentJob(1)}
-                        className={`border-l-2 ${currentJob === 1 ? 'border-light-blue' : 'border-grey'} hover:bg-light-grey hover:cursor-pointer py-2 pr-2 pl-4`}
-                    >
-                        <span
-                            onClick={() => setCurrentJob(1)}
-                            className={`text-lg md:text-xl ${currentJob === 1 ? 'text-light-blue' : 'text-grey'}  hover:cursor-pointer`}
-                        >
-                            {jobs[1].company}
-                        </span>
-                    </div>
+                    ))}
                 </div>
 
                 <Job
