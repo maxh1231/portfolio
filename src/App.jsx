a65e05411dace54d57360e4858b597a49d358b3a
max <maxhumpherys@gmail.com>
2025-06-08 19:14:45 -0600

    migrate to vite

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Nav from './components/Nav';
+import Landing from './components/Landing';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+// import Sider from '../components/Sider';
+
+function App() {
+    return (
+        <section className='App mx-12 my-6 '>
+            <Nav />
+            <Landing />
+
+            <About />
+            <Experience />
+            <Projects />
+            <Contact />
+            <Footer />
+            {/* <Sider /> */}
+        </section>
+    );
+}
+
+export default App;
